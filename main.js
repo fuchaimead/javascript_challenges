11763069330c3e43e5a92962ad80c90548206a2d
FuChai Mead <mead.fuchai@gmail.com>
2018-03-23 11:35:04 -0600

    longest word

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-function LongestWord(sen) { 
+var longest = 0
 
-  // code goes here  
+function LongestWord(sen) { 
+  var words = sen.split(' ')
+  words.forEach(function(word){
+     word = word.replace(/[^\w\s]|_/g, "")
+     if (longest < word.length){
+       longest = word.length
+       sen = word
+     }
+  })
+  
+  
   return sen; 
          
 }
    
 // keep this function call here 
-LongestWord(readline());                            
+LongestWord("find the longest word in this sentence");     
