d3bcc7f6127451eb0d8e1e129d4ecc5bcd762573
FuChai Mead <mead.fuchai@gmail.com>
2018-03-23 15:03:19 -0600

    test out code

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-var longest = 0
-
-function LongestWord(sen) { 
-  var words = sen.split(' ')
-  words.forEach(function(word){
-     word = word.replace(/[^\w\s]|_/g, "")
-     if (longest < word.length){
-       longest = word.length
-       sen = word
-     }
-  })
-  
-  
-  return sen; 
-         
+document.getElementById('longest-submit').onclick=function() {
+  var longest = 0
+  var sentence = document.getElementById('longest-input').value.split(' ')
+    sentence.forEach(function(word){
+       word = word.replace(/[^\w\s]|_/g, "")
+       if (longest < word.length){
+          longest = word.length
+          sentence = word
+       }
+    })
+    console.log(sentence)
+    document.getElementById('longest-answer').innerHTML = "The longest word is:" + sentence        
 }
-   
-// keep this function call here 
-LongestWord("find the longest word in this sentence");     
+
