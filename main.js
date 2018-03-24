a59036f773ae5085a277f38ae273a95c2d43f6a7
FuChai Mead <mead.fuchai@gmail.com>
2018-03-24 10:54:06 -0600

    factorial challenge

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+
+//longest word in a sentence 
 document.getElementById('longest-submit').onclick=function() {
-  var longest = 0
-  var sentence = document.getElementById('longest-input').value.split(' ')
+  let longest = 0
+  let sentence = document.getElementById('longest-input').value.split(' ')
+  debugger
     sentence.forEach(function(word){
        word = word.replace(/[^\w\s]|_/g, "")
        if (longest < word.length){
@@ -8,7 +11,16 @@ document.getElementById('longest-submit').onclick=function() {
           sentence = word
        }
     })
-    console.log(sentence)
-    document.getElementById('longest-answer').innerHTML = "The longest word is:" + sentence        
+    document.getElementById('longest-answer').innerHTML = "The longest word is: " + sentence        
+}
+
+//factorial of a number 
+document.getElementById('factorial-submit').onclick=function() {
+  let num = document.getElementById('factorial-input').value
+  let factorial = 1
+  for ( i=1; i <= num; i++) { 
+    factorial = factorial * i;
+  }
+  document.getElementById('factorial-answer').innerHTML = `The factorial of ${num} is ` + factorial        
 }
 
