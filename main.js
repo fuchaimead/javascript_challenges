b25852b162d83793c7543809642138ec7b2516bf
FuChai Mead <mead.fuchai@gmail.com>
2018-03-27 15:32:16 -0600

    shift letters

--- main.js
+++ main.js
@@ -39,5 +39,30 @@ document.getElementById('reverse-submit').onclick = () => {
   document.getElementById('reverse-answer').innerHTML = 'Reversed: ' + string        
 }
 
+//shift letters
+document.getElementById('letter-submit').onclick = () => {
+  let s = document.getElementById('letter-input').value.split('');
+    for (var i = 0; i < s.length; i++){
+      switch(s[i]){
+        case ' ':
+          break;
+        case 'z':
+          s[i] = 'a';
+          break;
+        case 'Z':
+          s[i] = 'A';
+          break;
+        default: 
+          s[i] = String.fromCharCode(1 + s[i].charCodeAt(0))
+      }
+        switch(s[i]) {
+          case 'a' : case 'e' : case 'i' : case 'o': case 'u' : 
+            s[i] = s[i].toUpperCase();
+          }    
+      }
+    document.getElementById('letter-answer').innerHTML = 'Reversed: ' + s.join('')        
+  }
+
+
 
 
